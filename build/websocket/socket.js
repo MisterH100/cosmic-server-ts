48ef675638d211f24bc30e75f730af94b78cab02
Misterh100 <handsomenyathi1@gmail.com>
2025-06-20 12:15:41 +0200

    chore: add timestamps to connected clients

--- build/websocket/socket.js
+++ build/websocket/socket.js
@@ -19,8 +19,18 @@ const io = new socket_io_1.Server(server, {
 });
 exports.io = io;
 io.on("connection", (socket) => {
-    console.log("user connected", socket.id);
+    const date = Date.now();
+    console.log(`user connected: ${socket.id} at ${parseTime(date)}`);
     socket.on("disconnect", () => {
-        console.log("user disconnected", socket.id);
+        console.log(`user disconnected: ${socket.id}`);
     });
 });
+const parseTime = (date) => {
+    const offsetMinutes = new Date(date).getTimezoneOffset();
+    const milliseconds = new Date(date).getTime();
+    const localTime = milliseconds - offsetMinutes * 60 * 1000;
+    // const seconds = Math.floor((localTime / 1000) % 60);
+    const minutes = Math.floor((localTime / 1000 / 60) % 60);
+    const hours = Math.floor((localTime / 1000 / 60 / 60) % 24);
+    return `${hours < 10 ? "0" : ""}${hours}:${minutes < 10 ? "0" : ""}${minutes} ${hours >= 12 ? "pm" : "am"}`;
+};
--- src/websocket/socket.ts
+++ src/websocket/socket.ts
@@ -11,12 +11,25 @@ const io = new Server(server, {
 })
 
 io.on("connection", (socket) => {
-  console.log("user connected", socket.id);
+  const date = Date.now();
+  console.log(`user connected: ${socket.id} at ${parseTime(date)}`);
   socket.on("disconnect", () => {
-    console.log("user disconnected", socket.id)
+    console.log(`user disconnected: ${socket.id}`)
   })
 
 });
+
+const parseTime = (date: number) => {
+  const offsetMinutes = new Date(date).getTimezoneOffset();
+  const milliseconds = new Date(date).getTime();
+  const localTime = milliseconds - offsetMinutes * 60 * 1000;
+  // const seconds = Math.floor((localTime / 1000) % 60);
+  const minutes = Math.floor((localTime / 1000 / 60) % 60);
+  const hours = Math.floor((localTime / 1000 / 60 / 60) % 24);
+
+  return `${hours < 10 ? "0" : ""}${hours}:${minutes < 10 ? "0" : ""
+    }${minutes} ${hours >= 12 ? "pm" : "am"}`;
+}
 
 export { app, io, server }
 
